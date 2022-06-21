0f5b2e5815f9dc08fc8fd3fa099f8df941388279
nmtuan <nmtuan@qq.com>
2022-06-21 19:53:41 +0800

    初始化

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+/*
+ * @Author: NMTuan
+ * @Email: NMTuan@qq.com
+ * @Date: 2022-06-21 19:41:22
+ * @LastEditTime: 2022-06-21 19:52:06
+ * @LastEditors: NMTuan
+ * @Description:
+ * @FilePath: \math_test_questions_generator\src\main.ts
+ */
+import App from './App.svelte'
+import 'uno.css'
+
+const app = new App({
+    target: document.getElementById('app')
+})
+
+export default app
