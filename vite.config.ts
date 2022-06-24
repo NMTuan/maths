513c15d8dbcecccca7d990434667b282fccef91b
NMTuan <nmtuan@qq.com>
2022-06-24 16:32:35 +0800

    docs

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@
  * @Author: NMTuan
  * @Email: NMTuan@qq.com
  * @Date: 2022-06-21 19:41:22
- * @LastEditTime: 2022-06-24 16:26:02
+ * @LastEditTime: 2022-06-24 16:32:23
  * @LastEditors: NMTuan
  * @Description:
  * @FilePath: \math_test_questions_generator\vite.config.ts
@@ -13,6 +13,7 @@ import Unocss from 'unocss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: './',
     plugins: [svelte(), Unocss()],
     build: {
         outDir: 'docs'
