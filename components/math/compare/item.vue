f265822713eee4e0781988ccaaa8af78ba5c382c
NMTuan <nmtuan@qq.com>
2023-02-27 11:01:29 +0800

    feat(math): 比大小

---
+++ components/math/compare/item.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="flex items-baseline flex-shrink-0 w-full sm:w-1/3 print:w-1/3 mb-4 text-lg">
+        <div class="text-sm text-gray-200 mr-4">
+            {{ (index + 1).toString().padStart(2, '0') }}.
+        </div>
+        <div class="flex items-center justify-center text-center">
+            <div class="w-10">{{ item.numbers[0] }}</div>
+            <div class="w-10 h-10 border rounded border-cool-gray-400 mx-2 text-3xl">
+                {{ showRes ? item.result : '' }}
+            </div>
+            <div class="w-10">{{ item.numbers[1] }}</div>
+        </div>
+    </div>
+</template>
+<script setup>
+const props = defineProps({
+    item: {
+        type: Object,
+        default: () => { }
+    },
+    index: {
+        type: Number,
+        default: 0
+    },
+    showRes: {
+        type: Boolean,
+        default: false
+    }
+})
+</script>
---
+++ pages/math/compare.vue
@@ -0,0 +1,115 @@
+<!--
+ * @Author: NMTuan
+ * @Email: NMTuan@qq.com
+ * @Date: 2023-02-27 10:33:15
+ * @LastEditTime: 2023-02-27 10:59:24
+ * @LastEditors: NMTuan
+ * @Description: 
+ * @FilePath: \ezMaths\pages\math\compare.vue
+-->
+<template>
+    <div>
+        <Title>{{ title }}</Title>
+        <LayoutPaper>
+            <template #config>
+                <div class="sm:flex items-center justify-between">
+                    <el-form class="flex items-center flex-wrap">
+                        <el-form-item label="运算范围" class="w-40 mr-4">
+                            <el-select v-model="currentRange" placeholder="" @change="submit">
+                                <el-option v-for="range in ranges" :key="range" :label="range" :value="range" />
+                            </el-select>
+                        </el-form-item>
+
+                        <el-form-item label="" class="">
+                            <el-checkbox v-model="showRes" label="显示答案" />
+                        </el-form-item>
+                    </el-form>
+
+                    <el-form class="flex-shrink-0 flex items-center flex-wrap">
+                        <el-form-item label="" class="">
+                            <el-button-group>
+                                <el-button type="primary" plain @click="submit">重新生成</el-button>
+                                <el-button type="primary" @click="print">打印</el-button>
+                            </el-button-group>
+                        </el-form-item>
+                    </el-form>
+                </div>
+            </template>
+            <div class="flex flex-wrap">
+                <MathCompareItem v-for="(item, index) in items" :item="item" :index="index" :showRes="showRes">
+                </MathCompareItem>
+            </div>
+        </LayoutPaper>
+    </div>
+</template>
+
+<script setup>
+const ranges = [10, 20, 50, 100] // 运算范围
+const currentRange = useCookie('math_compare_currentRange') // 当前运算范围
+currentRange.value = currentRange.value || 10
+
+const showRes = useCookie('math_compare_showRes') // 显示结果
+showRes.value = showRes.value || false
+
+const resLength = ref(50) // 生成数量
+const items = ref([]) // 结果集
+
+// 页面标题
+const title = computed(() => {
+    return `比大小`
+})
+
+// 生成随机数
+const random = (min = 0, max = currentRange.value) => {
+    return Math.round(Math.random() * (max - min)) + min
+}
+
+// 生成一条算数式
+
+const generator = () => {
+    const res = {
+        numbers: [], // 运算数
+        result: 0, // 结果
+    }
+
+    const a = random()
+    const b = random()
+
+    res.numbers = [a, b]
+    if (a === b) {
+        res.result = '='
+    }
+    if (a > b) {
+        res.result = '>'
+    }
+    if (a < b) {
+        res.result = '<'
+    }
+
+    return res
+}
+
+const submit = () => {
+    items.value = []
+    while (items.value.length < resLength.value) {
+        items.value.push(generator())
+    }
+}
+
+const print = () => {
+    window.print()
+}
+
+onMounted(() => {
+    submit()
+})
+</script>
+
+<script>
+export default {
+    page: {
+        sort: 600,
+        name: '比大小',
+    }
+}
+</script>
