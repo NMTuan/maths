c38635277e3757aea08b2a64414ccbfef3a45529
NMTuan <nmtuan@qq.com>
2023-03-28 17:28:55 +0800

    chore: 钟表配置项

--- components/math/clock/item.vue
+++ components/math/clock/item.vue
@@ -1,67 +1,67 @@
 <template>
     <div>
-    <div class="clock">
-        <div class="hour" :style="hourStyle"></div>
-        <div class="minute" :style="minuteStyle"></div>
-        <div class="notch">
-            <div class="cover"></div>
-            <div class="notch_1"></div>
-            <div class="notch_2"></div>
-            <div class="notch_3"></div>
-            <div class="notch_4"></div>
-            <div class="notch_5"></div>
-            <div class="notch_6"></div>
+        <div class="clock">
+            <div class="hour" :style="hourStyle"></div>
+            <div class="minute" :style="minuteStyle"></div>
+            <div class="notch">
+                <div class="cover"></div>
+                <div class="notch_1"></div>
+                <div class="notch_2"></div>
+                <div class="notch_3"></div>
+                <div class="notch_4"></div>
+                <div class="notch_5"></div>
+                <div class="notch_6"></div>
+            </div>
+            <div class="dot">
+                <div class="cover"></div>
+                <div class="dot_1"></div>
+                <div class="dot_2"></div>
+                <div class="dot_3"></div>
+                <div class="dot_4"></div>
+                <div class="dot_6"></div>
+                <div class="dot_7"></div>
+                <div class="dot_8"></div>
+                <div class="dot_9"></div>
+                <div class="dot_11"></div>
+                <div class="dot_12"></div>
+                <div class="dot_13"></div>
+                <div class="dot_14"></div>
+                <div class="dot_16"></div>
+                <div class="dot_17"></div>
+                <div class="dot_18"></div>
+                <div class="dot_19"></div>
+                <div class="dot_21"></div>
+                <div class="dot_22"></div>
+                <div class="dot_23"></div>
+                <div class="dot_24"></div>
+                <div class="dot_26"></div>
+                <div class="dot_27"></div>
+                <div class="dot_28"></div>
+                <div class="dot_29"></div>
+                <div class="dot_31"></div>
+                <div class="dot_32"></div>
+                <div class="dot_33"></div>
+                <div class="dot_34"></div>
+            </div>
+            <div class="number">
+                <div class="number_1"><span>{{ currentNumberType[1 - 1] }}</span><span>{{ currentNumberType[7 - 1] }}</span></div>
+                <div class="number_2"><span>{{ currentNumberType[2 - 1] }}</span><span>{{ currentNumberType[8 - 1] }}</span></div>
+                <div class="number_3"><span>{{ currentNumberType[3 - 1] }}</span><span>{{ currentNumberType[9 - 1] }}</span></div>
+                <div class="number_4"><span>{{ currentNumberType[4 - 1] }}</span><span>{{ currentNumberType[10 - 1] }}</span></div>
+                <div class="number_5"><span>{{ currentNumberType[5 - 1] }}</span><span>{{ currentNumberType[11 - 1] }}</span></div>
+                <div class="number_6"><span>{{ currentNumberType[6 - 1] }}</span><span>{{ currentNumberType[12 - 1] }}</span></div>
+            </div>
+            <div class="center"></div>
         </div>
-        <div class="dot">
-            <div class="cover"></div>
-            <div class="dot_1"></div>
-            <div class="dot_2"></div>
-            <div class="dot_3"></div>
-            <div class="dot_4"></div>
-            <div class="dot_6"></div>
-            <div class="dot_7"></div>
-            <div class="dot_8"></div>
-            <div class="dot_9"></div>
-            <div class="dot_11"></div>
-            <div class="dot_12"></div>
-            <div class="dot_13"></div>
-            <div class="dot_14"></div>
-            <div class="dot_16"></div>
-            <div class="dot_17"></div>
-            <div class="dot_18"></div>
-            <div class="dot_19"></div>
-            <div class="dot_21"></div>
-            <div class="dot_22"></div>
-            <div class="dot_23"></div>
-            <div class="dot_24"></div>
-            <div class="dot_26"></div>
-            <div class="dot_27"></div>
-            <div class="dot_28"></div>
-            <div class="dot_29"></div>
-            <div class="dot_31"></div>
-            <div class="dot_32"></div>
-            <div class="dot_33"></div>
-            <div class="dot_34"></div>
+        <div class="text-center my-4 text-lg">
+            {{ time }}
         </div>
-        <div class="number">
-            <div class="number_1"><span>{{currentNumberType[1-1]}}</span><span>{{currentNumberType[7-1]}}</span></div>
-            <div class="number_2"><span>{{currentNumberType[2-1]}}</span><span>{{currentNumberType[8-1]}}</span></div>
-            <div class="number_3"><span>{{currentNumberType[3-1]}}</span><span>{{currentNumberType[9-1]}}</span></div>
-            <div class="number_4"><span>{{currentNumberType[4-1]}}</span><span>{{currentNumberType[10-1]}}</span></div>
-            <div class="number_5"><span>{{currentNumberType[5-1]}}</span><span>{{currentNumberType[11-1]}}</span></div>
-            <div class="number_6"><span>{{currentNumberType[6-1]}}</span><span>{{currentNumberType[12-1]}}</span></div>
-        </div>
-        <div class="center"></div>
-    </div>
-    <div class="text-center my-4 text-lg">
-    {{ time }}
-    </div>
     </div>
 </template>
 <script setup>
 const numberType = [
-    { type: 'roman', arguments: ['1','2','3','4','5','6','7','8','9','10','11','12'] },
-    { type: 'arabic', arguments: ['I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII'] }
+    { type: 'roman', arguments: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'] },
+    { type: 'arabic', arguments: ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XI', 'XII'] }
 ]
 const currentNumberType = computed(() => {
     return numberType[0].arguments
@@ -89,7 +89,7 @@ const hourStyle = computed(() => {
 const minuteStyle = computed(() => {
     const rotate = minute.value * 6;
     return `transform: translate(-50%, -80%) rotate(${rotate}deg);`
-    
+
 })
 </script>
 <style lang="scss">
@@ -107,9 +107,11 @@ const minuteStyle = computed(() => {
     @apply absolute top-50% left-50% z-3;
     transform: translate(-50%, -50%) rotate($num * 30deg);
 }
+
 .notch {
     @apply absolute inset-0;
     @apply p-5%;
+
     .cover {
         background-color: #fff;
         width: 100%;
@@ -118,21 +120,27 @@ const minuteStyle = computed(() => {
         position: relative;
         border-radius: 100%;
     }
+
     &_1 {
         @include notch(1);
     }
+
     &_2 {
         @include notch(2);
     }
+
     &_3 {
         @include notch(3);
     }
+
     &_4 {
         @include notch(4);
     }
+
     &_5 {
         @include notch(5);
     }
+
     &_6 {
         @include notch(6);
     }
@@ -144,9 +152,11 @@ const minuteStyle = computed(() => {
     @apply absolute top-50% left-50% z-1;
     transform: translate(-50%, -50%) rotate($num * 6deg);
 }
+
 .dot {
     @apply absolute inset-0;
     @apply p-3%;
+
     .cover {
         background-color: #fff;
         width: 100%;
@@ -155,15 +165,19 @@ const minuteStyle = computed(() => {
         position: relative;
         border-radius: 100%;
     }
+
     &_1 {
         @include dot(1);
     }
+
     &_2 {
         @include dot(2);
     }
+
     &_3 {
         @include dot(3);
     }
+
     &_4 {
         @include dot(4);
     }
@@ -171,12 +185,15 @@ const minuteStyle = computed(() => {
     &_6 {
         @include dot(6);
     }
+
     &_7 {
         @include dot(7);
     }
+
     &_8 {
         @include dot(8);
     }
+
     &_9 {
         @include dot(9);
     }
@@ -184,12 +201,15 @@ const minuteStyle = computed(() => {
     &_11 {
         @include dot(11);
     }
+
     &_12 {
         @include dot(12);
     }
+
     &_13 {
         @include dot(13);
     }
+
     &_14 {
         @include dot(14);
     }
@@ -197,12 +217,15 @@ const minuteStyle = computed(() => {
     &_16 {
         @include dot(16);
     }
+
     &_17 {
         @include dot(17);
     }
+
     &_18 {
         @include dot(18);
     }
+
     &_19 {
         @include dot(19);
     }
@@ -210,36 +233,47 @@ const minuteStyle = computed(() => {
     &_21 {
         @include dot(21);
     }
+
     &_22 {
         @include dot(22);
     }
+
     &_23 {
         @include dot(23);
     }
+
     &_24 {
         @include dot(24);
     }
+
     &_26 {
         @include dot(26);
     }
+
     &_27 {
         @include dot(27);
     }
+
     &_28 {
         @include dot(28);
     }
+
     &_29 {
         @include dot(29);
     }
+
     &_31 {
         @include dot(31);
     }
+
     &_32 {
         @include dot(32);
     }
+
     &_33 {
         @include dot(33);
     }
+
     &_34 {
         @include dot(34);
     }
@@ -248,34 +282,44 @@ const minuteStyle = computed(() => {
 @mixin number($num) {
     transform: translate(-50%, -50%);
 }
+
 @mixin number($num) {
     transform: translate(-50%, -50%) rotate($num * 30deg);
+
     span {
         transform: rotate($num * -30deg);
     }
 }
+
 .number {
     @apply absolute inset-5%;
+
     div {
         @apply h-full text-center;
         @apply flex flex-col justify-between;
         @apply absolute top-50% left-50% z-5;
     }
+
     &_1 {
         @include number(1);
     }
+
     &_2 {
         @include number(2);
     }
+
     &_3 {
         @include number(3);
     }
+
     &_4 {
         @include number(4);
     }
+
     &_5 {
         @include number(5);
     }
+
     &_6 {
         @include number(6);
     }
@@ -292,6 +336,7 @@ const minuteStyle = computed(() => {
     transform: translate(-50%, -80%) rotate(60deg);
     z-index: 7;
 }
+
 .minute {
     width: 0;
     height: 40%;
@@ -304,10 +349,11 @@ const minuteStyle = computed(() => {
     transform: translate(-50%, -80%) rotate(30deg);
     z-index: 6;
 }
+
 .center {
     @apply w-0 h-0 overflow-hidden;
     @apply border-5 border-black rounded-full;
-    @apply absolute top-50% left-50% z-35;
+    @apply absolute top-50% left-50% z-8;
     transform: translate(-50%, -50%);
 }
 </style>
--- pages/math/clock.vue
+++ pages/math/clock.vue
@@ -2,47 +2,131 @@
  * @Author: NMTuan
  * @Email: NMTuan@qq.com
  * @Date: 2023-03-25 16:14:45
- * @LastEditTime: 2023-03-27 17:10:00
+ * @LastEditTime: 2023-03-28 17:27:38
  * @LastEditors: NMTuan
  * @Description: 
  * @FilePath: \ezMaths\pages\math\clock.vue
 -->
 <template>
     <LayoutPaper title="认识钟表">
-        <div class="flex flex-wrap">
-            <MathClockItem v-for="time in times" class="flex-1 m-4"
-            :time="time"
-            ></MathClockItem>
-        </div>
-        <div class="flex flex-wrap">
-            <MathClockItem v-for="time in times" class="flex-1 m-4"
-            :time="time"
-            ></MathClockItem>
-        </div>
-        <div class="flex flex-wrap">
-            <MathClockItem v-for="time in times" class="flex-1 m-4"
-            :time="time"
-            ></MathClockItem>
-        </div>
-        <div class="flex flex-wrap">
-            <MathClockItem v-for="time in times" class="flex-1 m-4"
-            :time="time"
-            ></MathClockItem>
+        <template #config>
+            <div class="sm:flex items-center justify-between">
+                <el-form class="flex items-center flex-wrap">
+                    <el-form-item label="模式" class="w-36 mr-4">
+                        <el-select v-model="currentTypeIndex" placeholder="请选择模式" @change="submit">
+                            <el-option v-for="(type, index) in types" :key="'type' + type.key" :label="type.label"
+                                :value="index" />
+                        </el-select>
+                    </el-form-item>
+
+                    <el-form-item label="表盘" class="w-38 mr-4">
+                        <el-select v-model="currentNumberTypeIndex" placeholder="请选择表盘" @change="submit">
+                            <el-option v-for="(type, index) in numberTypes" :key="'numberType' + type.key"
+                                :label="type.label" :value="index" />
+                        </el-select>
+                    </el-form-item>
+
+                    <el-form-item label="范围" class="w-36 mr-4">
+                        <el-select v-model="currentTimeTypeIndex" placeholder="请选择题目范围" @change="submit">
+                            <el-option v-for="(type, index) in timeTypes" :key="'numberType' + type.key" :label="type.label"
+                                :value="index" />
+                        </el-select>
+                    </el-form-item>
+
+                    <el-form-item label="" class="" v-if="currentTypeIndex === 1">
+                        <el-checkbox v-model="h24" label="24计时法" />
+                    </el-form-item>
+                </el-form>
+                <el-form class="flex-shrink-0 flex items-center flex-wrap">
+                    <el-form-item label="" class="">
+                        <el-button-group>
+                            <el-button type="primary" plain @click="submit">重新生成</el-button>
+                            <el-button type="primary" @click="print">打印</el-button>
+                        </el-button-group>
+                    </el-form-item>
+                </el-form>
+            </div>
+        </template>
+        {{ times }}
+        <div class="flex flex-wrap" v-for="index in 5">
+            <div v-for="i in 4">【{{ (index - 1) * index }} + {{ i }} = {{ (index - 1) * index + i }}】</div>
+            <!-- <MathClockItem v-for="i in 4" class="flex-1 mx-5" :time="times[(index - 1) * i]"></MathClockItem> -->
         </div>
     </LayoutPaper>
 </template>
 <script setup>
+const { $random } = useNuxtApp()
 const { $getSeoInfo } = useNuxtApp()
 const seo = $getSeoInfo()
 useServerSeoMeta(seo)
 useHead(seo)
 
-const times = [
+const times = ref([
     '12:00',
     '8:45',
-    '18:00',
+    '14:23',
     '3:30'
+])
+
+const types = [
+    { key: 'fill', label: '认识时间' },
+    { key: 'draw', label: '刻画时间' },
 ]
+const currentTypeIndex = useCookie('math_clock_currentTypeIndex')
+currentTypeIndex.value = currentTypeIndex.value || 0
+const type = computed(() => {
+    return types[currentTypeIndex.value]
+})
+
+const numberTypes = [
+    { key: 'arabic', label: '阿拉伯数字' },
+    { key: 'roman', label: '罗马数字' },
+    { key: 'none', label: '无' },
+]
+const currentNumberTypeIndex = useCookie('math_clock_currentNumberTypeIndex')
+currentNumberTypeIndex.value = currentNumberTypeIndex.value || 0
+const numberType = computed(() => {
+    return numberType[currentNumberTypeIndex.value]
+})
+
+const timeTypes = [
+    { key: 'hour', label: '仅整点' },
+    { key: 'half', label: '整点半点' },
+    { key: 'any', label: '任意时间' }
+]
+const currentTimeTypeIndex = useCookie('math_clock_currentTimeTypeIndex')
+currentTimeTypeIndex.value = currentTimeTypeIndex.value || 2
+const timeType = computed(() => {
+    return timeType[currentNumberTypeIndex.value]
+})
+
+// 24计时法
+const h24 = useCookie('math_clock_h24')
+h24.value = h24.value || false
+
+const generator = () => {
+    const hour = $random(0, h24.value ? 23 : 11)
+    let minute = 0
+    if (currentTimeTypeIndex.value === 1) {
+        minute = $random(0, 1) ? 0 : 30
+    }
+    if (currentTimeTypeIndex.value === 2) {
+        minute = $random(0, 59)
+    }
+    return `${hour}:${minute.toString().padStart(2, '0')}`
+}
+
+const submit = () => {
+    times.value = []
+    for (let i = 0; i < 20; i++) {
+        times.value.push(generator())
+    }
+}
+const print = () => {
+
+    window.print()
+}
+
 </script>
 
 <script>
